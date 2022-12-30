58dba45082841b33130b1976a6420137444bc51c
Anil Kumar <45123631+remoteservice1979@users.noreply.github.com>
2022-12-30 15:20:50 +0530

    Add information
    Add information

--- src/OrgChart.js
+++ src/OrgChart.js
@@ -270,7 +270,7 @@ oj.visualization.OrgChart = (function() {
                     properties["attributes"]["colspan"] = this.dimensionMatrix[i][j];
                     var element = this.htmlContent[i][j] = createNode("td", properties);
                     if (i == 1 && j == 1) {
-                        element.innerHTML = this.name;
+                        element.innerHTML = this.name + "    Gram & Present Address " + this.data;
                         element.onclick = oj.utilities.Events.bindHandler(nodeClickHandler, this);
                         element.ondblclick = oj.utilities.Events.bindHandler(nodeDblClickHandler, this);
                         element.onmouseover = oj.utilities.Events.bindHandler(mouseOverHandler, this);
