c3995daed67862258347c40a68989ead6b208ccc
Anil Kumar <45123631+remoteservice1979@users.noreply.github.com>
2023-01-02 10:29:53 +0530

    Update OrgChart.js
    FB button

--- src/OrgChart.js
+++ src/OrgChart.js
@@ -289,6 +289,11 @@ oj.visualization.OrgChart = (function() {
                             <p> ${this.data}</p>
                              <h4><b> Mobile # </b></h4>
                             <p> ${this.mobile}</p>
+                             <a href="#" class="fa fa-facebook"></a>
+                            <a href="#" class="fa fa-twitter"></a>
+                            <a href="#" class="fa fa-google"></a>
+                            <a href="#" class="fa fa-linkedin"></a>
+                            <a href="#" class="fa fa-youtube"></a>
                           </div>
                         </div>`
                         element.innerHTML = br;
